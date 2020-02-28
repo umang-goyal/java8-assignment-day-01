9ca3aa1e4cf13380ff3ef3481c530f40221266bb
umang-goyal <goel97umang@gmail.com>
2020-02-28 10:50:47 +0530

    improved few variable names

--- src/main/java/com/knoldus/Utilities.java
+++ src/main/java/com/knoldus/Utilities.java
@@ -6,13 +6,14 @@ import java.util.stream.IntStream;
 
 public class Utilities {
     int perimeter() {
-        Pentagon shape = (a, b, c, d, e) -> a + b + c + d + e;
+        Pentagon shape = (side1, side2, side3, side4, side5) -> side1 + side2 + side3 + side4 + side5;
         return shape.perimeter(1, 2, 3, 4, 5);
     }
 
+
     List<User> ageAbove18andDelhi(List<User> list) {
         return list.stream()
-                .filter(user -> user.age > 18 && user.address.equals("Delhi"))
+                .filter(user -> user.age > 18 && user.address.toLowerCase().equals("delhi"))
                 .collect(Collectors.toList());
     }
 
@@ -31,7 +32,7 @@ public class Utilities {
     List<User> age18AddressDelhi(List<User> list) {
         return list.stream()
                 .filter(user -> user.age == 18 && user.address.toLowerCase()
-                .equals("Delhi".toLowerCase()))
+                .equals("delhi"))
                 .collect(Collectors.toList());
     }
 
